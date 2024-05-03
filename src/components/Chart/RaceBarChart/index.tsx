7c5631623bae06b8b4391139c95c26e92f1f4d8a
10x10toddkim <sungjinkim001@10x10.co.kr>
2024-05-03 14:05:11 +0900

    update race bar chart data.

--- src/components/Chart/RaceBarChart/index.tsx
+++ src/components/Chart/RaceBarChart/index.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef } from 'react'
 import {
-    select, ascending, descending, rollup, pairs,
+    select, ascending, descending, rollup, pairs, groups, interpolateNumber, format, utcFormat,
+    axisTop,
+    scaleOrdinal, schemeTableau10
 } from 'd3'
 import json from './json/data.json'
 import './styles.css'
@@ -16,10 +18,16 @@ const RaceBarChart = () => {
         return data
     }
 
+    const getDateFormat = (date) => {
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        return year + '-' + month + '-' + day;
+    }
+
     const getKeyframes = () => {
-        console.log(Array.from(rollup(json, ([d]) => d.value, d => +d.date, d => d.name)))
-        const datevalues = Array.from(rollup(json, ([d]) => d.value, d => +d.date, d => d.name))
-            .map(([date, data]) => [new Date(date), data])
+        const datevalues = Array.from(rollup(json, ([d]) => d.value, d => d.date, d => d.name))
+            .map(([date, data]) => [getDateFormat(new Date(date)), data])
             .sort(([a], [b]) => ascending(a, b))
         const keyframes = [];
         let ka, a, kb, b;
@@ -36,13 +44,119 @@ const RaceBarChart = () => {
         return keyframes
     }
 
+    const bars = (svg, prev, next) => {
+        const color = () => {
+            const scale = scaleOrdinal(schemeTableau10)
+            if (json.some(d => d.category !== undefined)) {
+                const categoryByName = new Map(json.map(d => [d.name, d.category]))
+                scale.domain(categoryByName.values())
+                return d => scale(categoryByName.get(d.name))
+            }
+            return d => scale(d.name)
+        }
+        const bar = svg.append('g').attr('fill-opacity', 0.6).selectAll('rect')
+        return ([date, data], transition) => (bar) = bar
+            .data(data.slice(0, 12), d => d.name)
+            .join(
+                enter => enter.append('rect')
+                    .attr('fill', color)
+                    .attr('height', y.bandwidth())
+                    .attr('x', x(0))
+                    .attr('y', d => y((prev.get(d) || d).rank))
+                    .attr('width', d => x((prev.get(d) || d).value) - x(0)),
+                update => update,
+                exit => exit.transition(transition).remove()
+                    .attr('y', d => y((next.get(d) || d).rank))
+                    .attr('width', d => x((next.get(d) || d).value) - x(0))
+            )
+            .call(bar => bar.transition(transition)
+                .attr('y', d => y(d.rank))
+                .attr('width', d => x(d.value) - x(0)));
+    }
+
+    const axis = (svg) => {
+        const g = svg.append('g').attr('transform', `translate(0,${marginTop})`);
+        const axis = axisTop(x)
+            .ticks(width / 160, tickFormat)
+            .tickSizeOuter(0)
+            .tickSizeInner(-barSize * (12 + y.padding()))
+
+        return (_, transition) => {
+            g.transition(transition).call(axis)
+            g.select('.tick:first-of-type text').remove()
+            g.selectAll('.tick:not(:first-of-type) line').attr('stroke', 'white')
+            g.select('.domain').remove()
+        };
+    }
+
+    const labels = (svg, prev, next) => {
+        let label = svg.append('g')
+            .style('font', 'bold 12px var(--sans-serif)')
+            .style('font-variant-numeric', 'tabular-nums')
+            .attr('text-anchor', 'end')
+            .selectAll('text')
+
+        return ([date, data], transition) => label = label
+            .data(data.slice(0, 12), d => d.name)
+            .join(
+                enter => enter.append('text')
+                    .attr('transform', d => `translate(${x((prev.get(d) || d).value)},${y((prev.get(d) || d).rank)})`)
+                    .attr('y', y.bandwidth() / 2)
+                    .attr('x', -6)
+                    .attr('dy', '-0.25em')
+                    .text(d => d.name)
+                    .call(text => text.append('tspan')
+                        .attr('fill-opacity', 0.7)
+                        .attr('font-weight', 'normal')
+                        .attr('x', -6)
+                        .attr('dy', '1.15em')),
+                update => update,
+                exit => exit.transition(transition).remove()
+                    .attr('transform', d => `translate(${x((next.get(d) || d).value)},${y((next.get(d) || d).rank)})`)
+                    .call(g => g.select('tspan').tween('text', d => textTween(d.value, (next.get(d) || d).value)))
+            )
+            .call(bar => bar.transition(transition)
+                .attr('transform', d => `translate(${x(d.value)},${y(d.rank)})`)
+                .call(g => g.select('tspan').tween('text', d => textTween((prev.get(d) || d).value, d.value))))
+    }
+
+    const textTween = (a, b) => {
+        const i = interpolateNumber(a, b);
+        return function(t) {
+            this.textContent = format(',d')
+            // this.textContent = formatNumber(i(t));
+        };
+    }
+
+    const ticker = (svg) => {
+        const now = svg.append('text')
+            .style('font', `bold ${barSize}px var(--sans-serif)`)
+            .style('font-variant-numeric', 'tabular-nums')
+            .attr('text-anchor', 'end')
+            .attr('x', width - 6)
+            .attr('y', marginTop + barSize * (12 - 0.45))
+            .attr('dy', '0.32em')
+            .text(formatDate(keyframes[0][0]))
+
+        return ([date], transition) => {
+            // transition.end().then(() => now.text(formatDate(date)));
+            transition.end().then(() => now.text(utcFormat('%Y')))
+        };
+    }
+
     useEffect(() => {
         (async () => {
             if (json) {
                 const svg = select(ref.current)
-
                 const keyframes = getKeyframes()
-                console.log(keyframes)
+                const nameframes = groups(keyframes.flatMap(([, data]) => data), d => d.name)
+                const prev = new Map(nameframes.flatMap(([, data]) => pairs(data, (a, b) => [b, a])))
+                const next = new Map(nameframes.flatMap(([, data]) => pairs(data)))
+
+                const updateBars = bars(svg, prev, next)
+                const updateAxis = axis(svg)
+                const updateLabels = labels(svg, prev, next)
+                const updateTicker = ticker(svg)
             }
         })()
     }, [ref, json])
